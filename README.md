32b58cb48d5c84f1df003a16fb1e2e813e8d3121
kmkoni <25508476+kmkoni@users.noreply.github.com>
2022-04-07 08:56:06 -0400

    Latest version of script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Optimizely API Export to CSV
  Convert a set of parquet from the Optimizely API to easy to consume CSV
+
+# Requirements
+ - Python
+ - parquet-tools
+ - gawk
---
+++ opt-process.py
@@ -0,0 +1,38 @@
+import sys,os,shutil,json,csv
+
+print("Converting Optimizely API output to CSV...")
+print("» Collect all Parquet files")
+rdir = os.getcwd()
+
+for root, subdirs, files in os.walk(rdir):
+    for file in files:
+        path = os.path.join(root, file)
+        if os.path.join(root) != rdir:
+            shutil.copy(path, os.path.join(rdir))
+
+print("» Merge Parquet files")
+os.system('java --illegal-access=deny -jar /usr/local/bin/parquet-tools-1.11.0.jar merge *.parquet merged.parquet > /dev/null')
+
+print ("» Convert JSON to CSV")
+os.system('java --illegal-access=deny -jar /usr/local/bin/parquet-tools-1.11.0.jar cat -j merged.parquet > merged.json')
+
+# parquet-tools -j outputs multiple objects, we need to merge them into one
+os.system(r'''echo '[' `gawk -v RS= '{gsub(/}\n{/,"},{")} 1' merged.json` ']' > merged-fixed.json''')
+
+with open('merged-fixed.json') as json_file:
+    jsondata = json.load(json_file)
+
+data_file = open('output.csv', 'w', newline='')
+csv_writer = csv.writer(data_file)
+ 
+count = 0
+for data in jsondata:
+    if count == 0:
+        header = data.keys()
+        csv_writer.writerow(header)
+        count += 1
+    csv_writer.writerow(data.values())
+ 
+data_file.close()    
+
+print ("Done!")
